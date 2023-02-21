f6f944bd22d7a5c318f8fd29c095304ccf2e0f04
Cecily Rowland <cec1rowland@gmail.com>
2023-02-21 18:09:04 +0000

    Added view files for different pages and layout file

--- app.rb
+++ app.rb
@@ -2,9 +2,22 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
+
   configure :development do
     register Sinatra::Reloader
+  end
+
+  enable :sessions
+
+  get '/' do
+    return erb(:index)
+  end
+
+  get '/cv' do 
+    return erb(:cv)
+  end
+
+  get '/cover-letter' do
+    return erb(:cover_letter)
   end
 end
