c61b7cfaa2f8d2ee106df8053b3f38236d832c18
Cecily Rowland <cec1rowland@gmail.com>
2023-03-01 11:20:37 +0000

    Added cover letter content

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -9,8 +9,19 @@ describe Application do
   let(:app) { Application.new }
 
   context 'GET /' do
-    it 'should get the homepage' do
+    it 'should return the homepage' do
       response = get('/')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include "Hi, I'm Cecily!"
+      expect(response.body).to include "I'm a software developer living in London."
+      expect(response.body).to include "Click on the links to find out more about me and to see examples of my work."
+    end
+  end
+
+  context 'GET /cv' do
+    it 'should return the cv page' do
+      response = get('/cv')
 
       expect(response.status).to eq(200)
       expect(response.body).to include "Hi, I'm Cecily!"
